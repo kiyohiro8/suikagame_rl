801972f65cd2e10ee726d9af55cb663dc1c574d0
HiroshiKiyota <hkiyo8@gmail.com>
2024-05-20 02:22:20 +0900

    initial commit

---
+++ Commands/Keys.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+from collections import OrderedDict
+from enum import IntFlag, IntEnum, Enum, auto
+
+class Button(IntFlag):
+	Y = auto()
+	B = auto()
+	A = auto()
+	X = auto()
+	L = auto()
+	R = auto()
+	ZL = auto()
+	ZR = auto()
+	MINUS = auto()
+	PLUS = auto()
+	LCLICK = auto()
+	RCLICK = auto()
+	HOME = auto()
+	CAPTURE = auto()
+
+class Hat(IntEnum):
+	TOP			= 0
+	TOP_RIGHT	= 1
+	RIGHT 		= 2
+	BTM_RIGHT 	= 3
+	BTM 		= 4
+	BTM_LEFT 	= 5
+	LEFT 		= 6
+	TOP_LEFT 	= 7
+	CENTER 		= 8
+
+class Stick(Enum):
+	LEFT = auto()
+	RIGHT = auto()
+
+class Tilt(Enum):
+	UP = auto()
+	RIGHT = auto()
+	DOWN = auto()
+	LEFT = auto()
+	R_UP = auto()
+	R_RIGHT = auto()
+	R_DOWN = auto()
+	R_LEFT = auto()
+
+# direction value definitions
+min = 0
+center = 128
+max = 255
+
+# serial format
+class SendFormat:
+	def __init__(self):
+		# This format structure needs to be the same as the one written in Joystick.c
+		self.format = OrderedDict([
+			('btn', 0),	# send bit array for buttons
+			('hat', Hat.CENTER),
+			('lx', center),
+			('ly', center),
+			('rx', center),
+			('ry', center),
+		])
+
+		self.L_stick_changed = False
+		self.R_stick_changed = False
+
+	def setButton(self, btns):
+		for btn in btns:
+			self.format['btn'] |= btn
+	
+	def unsetButton(self, btns):
+		for btn in btns:
+			self.format['btn'] &= ~btn
+	
+	def resetAllButtons(self):
+		self.format['btn'] = 0
+	
+	def setHat(self, btns):
+		if not btns:
+			self.format['hat'] = Hat.CENTER
+		else:
+			self.format['hat'] = btns[0] # takes only first element
+	
+	def unsetHat(self):
+		self.format['hat'] = Hat.CENTER
+	
+	def setAnyDirection(self, dirs):
+		for dir in dirs:
+			if dir.stick == Stick.LEFT:
+				if self.format['lx'] != dir.x or self.format['ly'] != 255 - dir.y:
+					self.L_stick_changed = True
+
+				self.format['lx'] = dir.x
+				self.format['ly'] = 255 - dir.y # NOTE: y axis directs under
+			elif dir.stick == Stick.RIGHT:
+				if self.format['rx'] != dir.x or self.format['ry'] != 255 - dir.y:
+					self.R_stick_changed = True
+
+				self.format['rx'] = dir.x
+				self.format['ry'] = 255 - dir.y
+
+	def unsetDirection(self, dirs):
+		if Tilt.UP in dirs or Tilt.DOWN in dirs:
+			self.format['ly'] = center
+			self.format['lx'] = self.fixOtherAxis(self.format['lx'])
+			self.L_stick_changed = True
+		if Tilt.RIGHT in dirs or Tilt.LEFT in dirs:
+			self.format['lx'] = center
+			self.format['ly'] = self.fixOtherAxis(self.format['ly'])
+			self.L_stick_changed = True
+		if Tilt.R_UP in dirs or Tilt.R_DOWN in dirs:
+			self.format['ry'] = center
+			self.format['rx'] = self.fixOtherAxis(self.format['rx'])
+			self.R_stick_changed = True
+		if Tilt.R_RIGHT in dirs or Tilt.R_LEFT in dirs:
+			self.format['rx'] = center
+			self.format['ry'] = self.fixOtherAxis(self.format['ry'])
+			self.R_stick_changed = True
+	
+	# Use this to fix an either tilt to max when the other axis sets to 0
+	def fixOtherAxis(self, fix_target):
+		if fix_target == center:
+			return center
+		else:
+			return 0 if fix_target < center else 255
+	
+	def resetAllDirections(self):
+		self.format['lx'] = center
+		self.format['ly'] = center
+		self.format['rx'] = center
+		self.format['ry'] = center
+		self.L_stick_changed = True
+		self.R_stick_changed = True
+
+	def convert2str(self):
+		str_format = ''
+		str_L = ''
+		str_R = ''
+		space = ' '
+
+		# set bits array with stick flags
+		send_btn = int(self.format['btn']) << 2
+		if self.L_stick_changed:
+			send_btn |= 0x2
+			str_L = format(self.format['lx'], 'x') + space + format(self.format['ly'], 'x')
+		if self.R_stick_changed:
+			send_btn |= 0x1
+			str_R = format(self.format['rx'], 'x') + space + format(self.format['ry'], 'x')
+
+		str_format = format(send_btn, 'x') +\
+			(space + str(int(self.format['hat']))) +\
+			(space + str_L if self.L_stick_changed else '') +\
+			(space + str_R if self.R_stick_changed else '')
+
+		self.L_stick_changed = False
+		self.R_stick_changed = False
+
+		return str_format # the last space is not needed
+
+# This class handle L stick and R stick at any angles
+class Direction:
+	def __init__(self, stick, angle, isDegree=True, showName=None):
+		self.stick = stick	
+		self.angle_for_show = angle
+		self.showName = showName
+
+		if isinstance(angle, tuple):
+			# assuming (X, Y)
+			self.x = angle[0]
+			self.y = angle[1]
+			self.showName = '(' + str(self.x) + ', ' + str(self.y) + ')'
+		else:
+			angle = math.radians(angle) if isDegree else angle
+
+			# We set stick X and Y from 0 to 255, so they are calculated as below.
+			# X = 127.5*cos(theta) + 127.5
+			# Y = 127.5*sin(theta) + 127.5
+			self.x = math.ceil(127.5 * math.cos(angle) + 127.5)
+			self.y = math.floor(127.5 * math.sin(angle) + 127.5)
+
+	def __repr__(self):
+		if self.showName:
+			return "<{}, {}>".format(self.stick, self.showName)
+		else:
+			return "<{}, {}[deg]>".format(self.stick, self.angle_for_show)
+
+	def __eq__(self, other):
+		if (not type(other) is Direction):
+			return False
+
+		if self.stick == other.stick and self.angle_for_show == other.angle_for_show:
+			return True
+		else:
+			return False
+
+	def getTilting(self):
+		tilting = []
+		if self.stick == Stick.LEFT:
+			if self.x < center:		tilting.append(Tilt.LEFT)
+			elif self.x > center:	tilting.append(Tilt.RIGHT)
+
+			if self.y < center-1:	tilting.append(Tilt.DOWN)
+			elif self.y > center-1:	tilting.append(Tilt.UP)
+		elif self.stick == Stick.RIGHT:
+			if self.x < center:		tilting.append(Tilt.R_LEFT)
+			elif self.x > center:	tilting.append(Tilt.R_RIGHT)
+
+			if self.y < center-1:	tilting.append(Tilt.R_DOWN)
+			elif self.y > center-1:	tilting.append(Tilt.R_UP)
+		return tilting
+
+# Left stick for ease of use
+Direction.UP = Direction(Stick.LEFT, 90, showName='UP')
+Direction.RIGHT = Direction(Stick.LEFT, 0, showName='RIGHT')
+Direction.DOWN = Direction(Stick.LEFT, -90, showName='DOWN')
+Direction.LEFT = Direction(Stick.LEFT, -180, showName='LEFT')
+Direction.UP_RIGHT = Direction(Stick.LEFT, 45, showName='UP_RIGHT')
+Direction.DOWN_RIGHT = Direction(Stick.LEFT, -45, showName='DOWN_RIGHT')
+Direction.DOWN_LEFT = Direction(Stick.LEFT, -135, showName='DOWN_LEFT')
+Direction.UP_LEFT = Direction(Stick.LEFT, 135, showName='UP_LEFT')
+# Right stick for ease of use
+Direction.R_UP = Direction(Stick.RIGHT, 90, showName='UP')
+Direction.R_RIGHT = Direction(Stick.RIGHT, 0, showName='RIGHT')
+Direction.R_DOWN = Direction(Stick.RIGHT, -90, showName='DOWN')
+Direction.R_LEFT = Direction(Stick.RIGHT, -180, showName='LEFT')
+Direction.R_UP_RIGHT = Direction(Stick.RIGHT, 45, showName='UP_RIGHT')
+Direction.R_DOWN_RIGHT = Direction(Stick.RIGHT, -45, showName='DOWN_RIGHT')
+Direction.R_DOWN_LEFT = Direction(Stick.RIGHT, -135, showName='DOWN_LEFT')
+Direction.R_UP_LEFT = Direction(Stick.RIGHT, 135, showName='UP_LEFT')
+
+# handles serial input to Joystick.c
+class KeyPress:
+	def __init__(self, ser):
+		self.ser = ser
+		self.format = SendFormat()
+		self.holdButton = []
+	
+	def input(self, btns):
+		if not isinstance(btns, list):
+			btns = [btns]
+		
+		for btn in self.holdButton:
+			if not btn in btns:
+				btns.append(btn)
+
+		# print to log
+		#print(btns)
+
+		self.format.setButton([btn for btn in btns if type(btn) is Button])
+		self.format.setHat([btn for btn in btns if type(btn) is Hat])
+		self.format.setAnyDirection([btn for btn in btns if type(btn) is Direction])
+		self.ser.writeRow(self.format.convert2str())
+
+	def inputEnd(self, btns):
+		if not isinstance(btns, list):
+			btns = [btns]
+
+		# get tilting direction from angles
+		tilts = []
+		for dir in [btn for btn in btns if type(btn) is Direction]:
+			tiltings = dir.getTilting()
+			for tilting in tiltings:
+				tilts.append(tilting)
+
+		self.format.unsetButton([btn for btn in btns if type(btn) is Button])
+		self.format.unsetHat()
+		self.format.unsetDirection(tilts)
+
+		self.ser.writeRow(self.format.convert2str())
+
+	def hold(self, btns):
+		if not isinstance(btns, list):
+			btns = [btns]
+
+		for btn in btns:
+			if btn in self.holdButton:
+				print('Warning: ' + btn.name + ' is already in holding state')
+				return
+			
+			self.holdButton.append(btn)
+		
+		self.input(btns)
+		
+	def holdEnd(self, btns):
+		if not isinstance(btns, list):
+			btns = [btns]
+		
+		for btn in btns:
+			self.holdButton.remove(btn)
+
+		self.inputEnd(btns)
+	
+	def end(self):
+		self.ser.writeRow('end')
---
+++ env.py
@@ -0,0 +1,254 @@
+# スイカゲームプレイ用の環境
+# Stateはcv2.VideoCapture(2)で取得した画像
+# Actionは
+import time
+
+import cv2
+import easyocr
+
+from Commands.Keys import KeyPress, Button, Hat, Direction, Stick
+from utils import Sender
+
+COMPORT = 3
+SCORE_XMIN, SCORE_YMIN, SCORE_XMAX, SCORE_YMAX = 150, 160, 400, 260
+RING_EVOLUTION_XMIN, RING_EVOLUTION_YMIN, RING_EVOLUTION_XMAX, RING_EVOLUTION_YMAX = 1400, 500, 1850, 1000
+RING_EVOLUTION_THRESHOLD = 400000000
+RANKIN_TEXT_XMIN, RANKIN_TEXT_YMIN, RANKIN_TEXT_XMAX, RANKIN_TEXT_YMAX = 600, 200, 1300, 300
+RANKIN_TEXT_XMIN_2, RANKIN_TEXT_YMIN_2, RANKIN_TEXT_XMAX_2, RANKIN_TEXT_YMAX_2 = 250, 150, 1100, 250
+GAMEOVER_TEXT_XMIN, GAMEOVER_TEXT_YMIN, GAMEOVER_TEXT_XMAX, GAMEOVER_TEXT_YMAX = 400, 150, 1000, 220
+RANKIN_THRESHOLD = 100000000
+RANKIN_THRESHOLD_2 = 100000000
+GAMEOVER_THRESHOLD = 100000000
+INIT_TEXT_XMIN, INIT_TEXT_YMIN, INIT_TEXT_XMAX, INIT_TEXT_YMAX = 400, 320, 1500, 420
+INIT_TEXT_THRESHOLD = 100000000
+OBSERVE_XMIN, OBSERVE_YMIN, OBSERVE_XMAX, OBSERVE_YMAX = 550, 50, 1750, 1050
+OBSERVE_SIZE = (120, 100)
+
+
+class SuikaEnv:
+    def __init__(self):
+
+        self.rankin_text_image = cv2.imread("template_images/rankin_text_image.png")
+        self.rankin_text_image_2 = cv2.imread("template_images/rankin_text_image_2.jpg")
+        self.gameover_text_image = cv2.imread("template_images/gameover_text_image.png")
+        self.init_text_image = cv2.imread("template_images/init_text_image.png")
+        self.ring_evolution_image = cv2.imread("template_images/ring_evolution.jpg")
+        self.rankin_text_image = self.rankin_text_image[:, :, ::-1]
+        self.rankin_text_image_2 = self.rankin_text_image_2[:, :, ::-1]
+        self.gameover_text_image = self.gameover_text_image[:, :, ::-1]
+        self.init_text_image = self.init_text_image[:, :, ::-1]
+        self.ring_evolution_image = self.ring_evolution_image[:, :, ::-1]
+        self.reader = easyocr.Reader(['en'])
+        self.current_score = 0
+        ser = Sender(False)
+        ser.openSerial(COMPORT)
+        self.key = KeyPress(ser)
+
+        self.cap = cv2.VideoCapture(2)
+        self.cap.set(3, 1920)
+        self.cap.set(4, 1080)
+        self.reader = easyocr.Reader(['en'])
+        self.current_score = 0
+
+    
+    def step(self, action):
+        if action == 0:
+            # Aを押す
+            self.key.input([Button.A])
+            time.sleep(0.1)
+            self.key.inputEnd([Button.A])
+            time.sleep(1)
+        elif action == 1:
+            # 左を押す
+            self.key.input([Hat.LEFT])
+            time.sleep(0.2)
+            self.key.inputEnd([Hat.LEFT])
+        elif action == 2:
+            # 右を押す
+            self.key.input([Hat.RIGHT])
+            time.sleep(0.2)
+            self.key.inputEnd([Hat.RIGHT])
+        else:
+            raise ValueError('action must be 0, 1 or 2')
+
+        # 点数の取得
+        score = self.get_score()
+        if score >= 0:
+            reward = score - self.current_score
+            if 0 < reward < 500: # スイカゲームの仕様上1秒間に500点以上増えることは通常ない
+                self.current_score = score
+            else:
+                reward = 0
+        else:
+            reward = 0
+        
+        # 終了判定を行う
+        done = self.is_done()
+
+        # 観測の取得
+        obs = self.get_observation()
+        return obs, reward, done
+
+    def reset(self):
+        # stateを初期化
+        obs = self.get_observation()
+        self.current_score = self.get_score()
+
+        return obs
+    
+    def return_to_game(self):
+        game_state = self.is_which_state()
+
+        while game_state != "Playing":
+            if game_state == "Rankin":
+                # Aを押して1秒待機
+                self.key.input([Button.A])
+                time.sleep(0.1)
+                self.key.inputEnd([Button.A])
+                time.sleep(2)
+                # 左を押す
+                self.key.input([Hat.LEFT])
+                time.sleep(0.2)
+                self.key.inputEnd([Hat.LEFT])
+                # Aを押す
+                self.key.input([Button.A])
+                time.sleep(0.1)
+                self.key.inputEnd([Button.A])
+                time.sleep(3)
+            elif game_state == "Rankin_2":
+                self.key.input([Button.A])
+                time.sleep(0.1)
+                self.key.inputEnd([Button.A])
+                time.sleep(2)
+                # 左を押す
+                self.key.input([Hat.LEFT])
+                time.sleep(0.2)
+                self.key.inputEnd([Hat.LEFT])
+                # Aを押す
+                self.key.input([Button.A])
+                time.sleep(0.1)
+                self.key.inputEnd([Button.A])
+                time.sleep(3)
+            elif game_state == "Gameover":
+                # 左を押す
+                self.key.input([Hat.LEFT])
+                time.sleep(1)
+                self.key.inputEnd([Hat.LEFT])
+                # Aを押す
+                self.key.input([Button.A])
+                time.sleep(0.1)
+                self.key.inputEnd([Button.A])
+                time.sleep(3)
+            elif game_state == "Init":
+                self.key.input([Button.L, Button.R])
+                time.sleep(6)
+                self.key.inputEnd([Button.L, Button.R])
+                self.key.input([Button.A])
+                time.sleep(0.1)
+                self.key.inputEnd([Button.A])
+                self.key.input([Button.A])
+                time.sleep(0.1)
+                self.key.inputEnd([Button.A])
+                self.key.input([Button.A])
+                time.sleep(0.1)
+                self.key.inputEnd([Button.A])
+                time.sleep(3)
+            time.sleep(1)
+            frame = self.get_frame() # get_frame()を空打ちしないと取得できるカメラ画像が更新されていないことがある（？）
+            game_state = self.is_which_state()
+    
+    def is_done(self):
+        # 現在の画面が状態かを判定するメソッド
+        frame = self.get_frame()
+        # 通常のプレイ画面かどうかの判定
+        res = cv2.matchTemplate(
+             frame[RING_EVOLUTION_YMIN:RING_EVOLUTION_YMAX, RING_EVOLUTION_XMIN:RING_EVOLUTION_XMAX, :], 
+             self.ring_evolution_image, 
+             cv2.TM_CCOEFF
+        )
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        if max_val > RING_EVOLUTION_THRESHOLD:
+            return False
+        else:
+            return True
+    
+    def is_which_state(self):
+        # 現在の画面が状態かを判定するメソッド
+        frame = self.get_frame()
+        # 通常のプレイ画面かどうかの判定
+        res = cv2.matchTemplate(
+             frame[RING_EVOLUTION_YMIN:RING_EVOLUTION_YMAX, RING_EVOLUTION_XMIN:RING_EVOLUTION_XMAX, :], 
+             self.ring_evolution_image, 
+             cv2.TM_CCOEFF
+        )
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        if max_val > RING_EVOLUTION_THRESHOLD:
+            return "Playing"
+
+        # ランキングイン画面かどうかの判定
+        res = cv2.matchTemplate(
+             frame[RANKIN_TEXT_YMIN:RANKIN_TEXT_YMAX, RANKIN_TEXT_XMIN:RANKIN_TEXT_XMAX, :], 
+             self.rankin_text_image, 
+             cv2.TM_CCOEFF
+        )
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        if max_val > RANKIN_THRESHOLD:
+            return "Rankin"
+        
+        # もう一つのランキングイン画面かどうかの判定
+        res = cv2.matchTemplate(
+             frame[RANKIN_TEXT_YMIN_2:RANKIN_TEXT_YMAX_2, RANKIN_TEXT_XMIN_2:RANKIN_TEXT_XMAX_2, :],
+             self.rankin_text_image_2, 
+             cv2.TM_CCOEFF
+        )
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        if max_val > RANKIN_THRESHOLD_2:
+            return "Rankin_2"
+
+        # ゲームオーバー画面かどうかの判定
+        res = cv2.matchTemplate(
+             frame[GAMEOVER_TEXT_YMIN:GAMEOVER_TEXT_YMAX, GAMEOVER_TEXT_XMIN:GAMEOVER_TEXT_XMAX, :], 
+             self.gameover_text_image, 
+             cv2.TM_CCOEFF
+        )
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        if max_val > GAMEOVER_THRESHOLD:
+            return "Gameover"
+        
+        res = cv2.matchTemplate(
+                frame, 
+                self.init_text_image, 
+                cv2.TM_CCOEFF
+            )
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        if max_val > INIT_TEXT_THRESHOLD:
+            return "Init"
+
+        return None
+    
+    def get_score(self):
+        # 現在のスコアを取得するメソッド
+        frame = self.get_frame()
+        score_image = frame[SCORE_YMIN:SCORE_YMAX, SCORE_XMIN:SCORE_XMAX, :]
+        text = self.reader.readtext(score_image)
+        try:
+            score = int(text[0][1])
+        except:
+            score = -1
+        return score
+
+    def get_frame(self, discard_frame=1):
+        for _ in range(discard_frame):
+            self.cap.read()
+        # 現在のフレームを取得するメソッド
+        ret, frame = self.cap.read()
+        if not ret:
+            raise ValueError('frame is None')
+        frame = frame[:, :, ::-1]
+        return frame
+    
+    def get_observation(self):
+        frame = self.get_frame()
+        obs = frame[OBSERVE_YMIN:OBSERVE_YMAX, OBSERVE_XMIN:OBSERVE_XMAX, :]
+        obs = cv2.resize(obs, OBSERVE_SIZE, interpolation=cv2.INTER_LINEAR)
+        return obs
